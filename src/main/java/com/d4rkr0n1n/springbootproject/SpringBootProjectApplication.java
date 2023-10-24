98b9c157c7aad6679344a7b7183c6bbca8e51819
Mridul Roy <midlry.mr@gmail.com>
2023-10-24 08:32:39 +0530

    first commit

---
+++ src/main/java/com/d4rkr0n1n/springbootproject/SpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.d4rkr0n1n.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d4rkr0n1n/springbootproject/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d4rkr0n1n.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
